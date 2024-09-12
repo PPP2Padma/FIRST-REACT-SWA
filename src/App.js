40dcb2c7fd2c6f9aa52b7d8ff48bf0398d82da48
priya <padma.2586@gmail.com>
2024-09-12 10:54:05 +0800

    test

--- src/App.js
+++ src/App.js
@@ -3,14 +3,9 @@ import React from 'react';
 function App() {
   const value = 'World test';
 
-  //return <div>Hello {value}</div>;
+  return <div>Hello {value}</div>;
 
-  return (
-    <div>
-      <h3>Account</h3>
-      
-    </div>
-)
+
 }
 
 export default App;
