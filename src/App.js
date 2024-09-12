89554e7841fc271fb319b1496fc192601b5ee3d9
priya <padma.2586@gmail.com>
2024-09-12 10:49:05 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 function App() {
   const value = 'World test';
-  return <div>Hello {value}</div>;
+
+  //return <div>Hello {value}</div>;
+
+  return (
+    <div>
+      <h3>Account</h3>
+      <a href="#" onClick={some_event}>Login</a>
+      <a href="#" onClick={some_event}>Logout</a>
+    </div>
+)
 }
 
 export default App;
